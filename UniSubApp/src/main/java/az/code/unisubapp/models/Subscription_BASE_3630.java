276d9a5266743ce2f3bc2dbdeacaf44755734ba5
owl <owl@owls-MacBook-Pro.local>
2021-06-11 12:48:52 +0400

    working models

---
+++ UniSubApp/src/main/java/az/code/unisubapp/models/Subscription_BASE_3630.java
@@ -0,0 +1,32 @@
+package az.code.unisubapp.models;
+
+import az.code.unisubapp.models.enums.Plan;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Entity(name = "Subs")
+@Table(name = "subscriptions")
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String item;
+    private Plan plan;
+    private BigDecimal price;
+    private Card card;
+    private LocalDate subDate;
+    private URL website;
+    private boolean isActive;
+    private LocalDate deactivatedDate;
+}
