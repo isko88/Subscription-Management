992c6daa9369ea98eaee108e6af640600d28c9bf
Vusal Abdullayev <Theabdullayev6>
2021-06-11 10:23:55 +0400

    new project

---
+++ UniSubApp/src/main/java/az/code/unisubapp/UniSubAppApplication.java
@@ -0,0 +1,13 @@
+package az.code.unisubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSubAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniSubAppApplication.class, args);
+    }
+
+}
---
+++ UniSubApp/src/test/java/az/code/unisubapp/UniSubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.code.unisubapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSubAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
