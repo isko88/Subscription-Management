6dc7767ddefb5972cba660a96435629f339cf1d4
emilpyp <emilnah@code.edu.az>
2021-06-13 00:24:28 +0400

    login register

--- UniSubApp/src/main/java/az/code/unisubapp/repository/AppUserRepository.java
+++ UniSubApp/src/main/java/az/code/unisubapp/repository/AppUserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppUserRepository extends JpaRepository<AppUser,Long> {
     AppUser getAppUserByUsername(String username);
+    AppUser getAppUserByUsernameAndPasswordEquals(String username, String password);
 }
